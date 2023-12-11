022b1fc7e9e580ea4c90264f7fc9883886fb71bf
Thisislearner <ak97plutonium@gmail.com>
2023-12-11 13:26:55 +0530

    added animation to button

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-`strict mode`
+`strict mode`;
+const message = document.querySelector(".message");
+
+const message_text = document.querySelector(".message_text");
+
+const display = document.querySelector(".display");
+
+const submit = document.querySelector(".submit");
+
+const reset = document.querySelector(".reset");
+
+//check for input
+document.addEventListener();
